47d251ea14fd7d92de08c9c831acebf9f17994c7
Richard Lupton <richard.lupton@gmail.com>
2016-05-21 11:17:12 +0100

    Made the tmux launcher script  identify whether it was running in X or not, and use percol when it's not.  Percol isn't perfect for this, since I haven't figured out yet if I can return a failed query, so I can create a new session when no named session exists.

--- .scripts/tmux-launcher.sh
+++ .scripts/tmux-launcher.sh
@@ -1,28 +1,62 @@
 #!/bin/bash
 
-############################################################
-# tmux session launcher  # # # # # # # # # # # # # # # # # #
-############################################################
+################################################################
+# tmux session launcher  # # # # # # # # # # # # # # # # # # # #
+################################################################
 
-############################################################
-# These variables configure a) the virtual (graphical)     #
-# terminal in use, along with the appropriate switch       #
-# to open the terminal running a particular                #
-# application, and b) the incantation for dmenu to         #
-# allow a particular tmux instance to be used. I use       #
-# dmenu2 which allows more configuration of appearances;   #
-# if this doesn't work, try setting SELECTOR to just       #
-# dmenu and then customize.                                #
-############################################################
+################################################################
+# The variables here select the programs which are used        #
+# in the background.                                           #
+#                                                              #
+#   XTERM :        the X virtual terminal (with appropriate    #
+#                  switches) to launch a new terminal process  #
+#                  in X and execute a parameter.               #
+#                                                              #
+#   XSELECTOR :    The program to use for selection of the     #
+#                  tmux seession to launch. This is set to     #
+#                  dmenu2 which has some aesthetic             #
+#                  improvements over ordinary dmenu. If using  #
+#                  dmenu, perhaps start with just "dmenu" and  #
+#                  customize from there.                       #
+#                                                              #
+#   TERMSELECTOR : Which selection program to use in a         #
+#                  pure terminal environment. This uses        #
+#                  percol (install with pip). Percol does      #
+#                  not return unmatched user input, so         #
+#                  (at the moment) the pure terminal           #
+#                  version of this script will not launch      #
+#                  a new session on a failed match :(.         #
+#                                                              #
+################################################################
 XTERM="urxvt -e"
-SELECTOR="dmenu -h 30 -y 350 -nb #24251f -nf #66d9ef \
-          -sb #272822 -sf #f92672 -fn LiberationMono-10:bold"
+XSELECTOR="dmenu -h 30 -y 350 \
+             -nb #24251f -nf #66d9ef -sb #272822 -sf #f92672 \
+             -fn LiberationMono-10:bold"
+TERMSELECTOR="percol"
+
+
+################################################################
+# We need to know whether we have an X display available, or   #
+# whether we are in a pure terminal environment. We set        #
+# some program variables accordingly.                          #
+################################################################
+if [ -z "$DISPLAY" ]; then
+    SELECTOR="$TERMSELECTOR"
+    RUN=""
+else
+    SELECTOR="$XSELECTOR"
+    RUN="$XTERM"
+fi
+
+################################################################
+# Now we are set up, we can make a choice and act accordingly. #
+################################################################
 
 # Make a choice of an existing session, or naming a new session
 CHOICE=$(tmux ls | awk '{ print $1 }' | sed 's\:.*\\' | $SELECTOR)
 
 # If no choice is made, exit now
-if [ "$CHOICE" = "" ]; then
+if [ -z "$CHOICE" ]; then
     exit
 fi
 
@@ -31,9 +65,9 @@ EXISTS=$(tmux ls | awk "/$CHOICE/ { print NR }")
 
 # If not, make a new one with this name, and in the
 # other case, attach to that session.
-if [ "$EXISTS" = "" ]; then
-    $XTERM tmux new -s $CHOICE
+if [ -z "$EXISTS" ]; then
+    $RUN tmux new -s $CHOICE
 else
-    $XTERM tmux attach-session -t $CHOICE
+    $RUN tmux attach-session -t $CHOICE
 fi
 
