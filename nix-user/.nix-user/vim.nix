0a973de1717124157d1c9920a6677591920717e7
Richard Lupton <richard.lupton@gmail.com>
2017-09-23 18:43:39 +0100

    (nix) vim-airline and python3 for vim

--- nix-user/.nix-user/vim.nix
+++ nix-user/.nix-user/vim.nix
@@ -1,22 +1,30 @@
-{pkgs ? import <nixpkgs> {} }:
-
+{pkgs ? import <nixpkgs> {}}:
+let
+  custom_vim_configurable = with pkgs;
+    vimUtils.makeCustomizable (vim_configurable.override { python = python3; });
+in
 with pkgs; rec {
-  custom-vim = vim_configurable.customize {
+  custom-vim = custom_vim_configurable.customize {
     name = "vim";
     vimrcConfig.customRC = ''
       syntax on
       colorscheme molokai
 
       set relativenumber
+
+      let g:airline_powerline_fonts = 1
+      let g:airline_theme='powerlineish'
+      set laststatus=2
     '';
     vimrcConfig.vam.knownPlugins = pkgs.vimPlugins;
     vimrcConfig.vam.pluginDictionaries = [
       { names = [
         "molokai"
+        "vim-airline"
+        "vim-airline-themes"
         "youcompleteme"
         "Syntastic"
       ]; }
-      ];
+    ];
   };
-
 }
